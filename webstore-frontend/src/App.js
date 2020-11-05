ed539dc2542a9c5a5864e244f9b8830637a07152
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2020-11-05 18:45:46 +0200

    added shopping cart functionality and local storage

--- webstore-frontend/src/App.js
+++ webstore-frontend/src/App.js
@@ -1,23 +1,31 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Link, Route } from 'react-router-dom'
 import './App.css'
+import {useSelector} from 'react-redux'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 
 function App() {
+    const cart = useSelector((state) => state.cart)
+
+    const { cartItems } = cart
+
     return (
         <BrowserRouter>
             <div className='grid-container'>
                 <header className='row'>
                     <div>
-                        <a className='brand' href='/'>
+                        <Link to='/' className='brand'>
                             Web store IV
-                        </a>
+                        </Link>
                     </div>
                     <div>
-                        <a href='/cart'>Cart</a>
-                        <a href='/signin'>Sign In</a>
+                        <Link to='/cart'>Cart</Link>
+                        {cartItems.length > 0 && (
+                            <span className='badge'>{cartItems.length}</span>
+                        )}
+                        <Link to='/signin'>Sign In</Link>
                     </div>
                 </header>
                 <main>
