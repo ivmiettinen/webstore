31b692b7b16a2f07a687d5c3f502b3a2e6564394
Ilkka Miettinen <ivmiettinen@gmail.com>
2021-04-12 12:27:22 +0300

    added order actions and reducers to frontend

--- webstore-frontend/src/screens/PlaceOrderScreen.js
+++ webstore-frontend/src/screens/PlaceOrderScreen.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { createOrder } from '../actions/orderActions';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { ORDER_CREATE_RESET } from '../constants/orderConstants';
+import LoadingBox from '../components/LoadingBox'
+import MessageBox from '../components/MessageBox'
 
 export default function PlaceOrderScreen(props) {
 
-
+  
+  const cart = useSelector((state) => state.cart);
   const userSignin = useSelector((state) => state.userSignin);
 
   if (userSignin.userInfo === null) {
     props.history.push('/signin');
   }
 
-  const cart = useSelector((state) => state.cart);
-
   if (!cart.paymentMethod) {
     props.history.push('/payment');
   }
+
+
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { loading, success, error, order } = orderCreate;
+
+ 
 
   const toPrice = (num) => Number(num.toFixed(2)); // 5.123 => "5.12" => 5.12
   cart.itemsPrice = toPrice(
@@ -25,9 +34,18 @@ export default function PlaceOrderScreen(props) {
   cart.shippingPrice = cart.itemsPrice > 100 ? toPrice(0) : toPrice(10);
   cart.taxPrice = toPrice(0.15 * cart.itemsPrice);
   cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+
+  const dispatch = useDispatch();
   const placeOrderHandler = () => {
-    // TODO: dispatch place order action
+    dispatch(createOrder({ ...cart, orderItems: cart.cartItems }));
   };
+  useEffect(() => {
+    if (success) {
+      props.history.push(`/order/${order._id}`);
+      dispatch({ type: ORDER_CREATE_RESET });
+    }
+  }, [dispatch, order, props.history, success]);
+
   return (
     <div>
       <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
@@ -127,6 +145,8 @@ export default function PlaceOrderScreen(props) {
                 >
                   Place Order
                 </button>
+                {loading && <LoadingBox></LoadingBox>}
+                {error && <MessageBox variant='danger'>{error}</MessageBox>}
               </li>
             </ul>
           </div>
