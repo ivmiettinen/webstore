b768750fe8596ab8e9902ece9ef9f3b3e34e45c3
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2021-04-20 14:27:02 +0300

    added paypal feature

--- webstore-backend/server.js
+++ webstore-backend/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 import productRouter from './routers/productRouter.js'
 import userRouter from './routers/userRouter.js'
 import orderRouter from './routers/orderRouter.js'
-import cors from 'cors'
+// import cors from 'cors'
 
 dotenv.config()
 
@@ -16,8 +16,8 @@ app.use(express.urlencoded({extended: true}))
 
 
 
-app.use(cors());
-app.options('*', cors());
+// app.use(cors());
+// app.options('*', cors());
 
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/webstore', {
@@ -42,6 +42,10 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/webstore', {
 app.use('/api/users', userRouter)
 app.use('/api/products', productRouter)
 app.use('/api/orders', orderRouter)
+
+app.get('/api/config/paypal', (req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID || 'sandbox')
+})
 
 app.get('/', (req, res) => {
     res.send('Server running')
